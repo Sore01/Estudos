cec7d5938b172be17a39fc042c8fe8bee64ccb79
Eros Lestat <137727713+Sore01@users.noreply.github.com>
2024-09-10 15:50:37 -0300

    Escreva Algo

---
+++ EscrevaAlgo/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(" Escreva algo  ");
+        String input = scanner.nextLine();
+
+        System.out.println(" nao sei " +input );
+
+        scanner.close();
+    }
+}
